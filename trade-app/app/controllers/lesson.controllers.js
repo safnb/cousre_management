ed99a0ce94905352e2b4cbe57cfa5487cb52ac86
Сафие Небиева <safienebieva@MacBook-Air-Safie.local>
2025-11-29 20:22:50 +0300

    Lesson controller added

---
+++ trade-app/app/controllers/lesson.controllers.js
@@ -0,0 +1,136 @@
+const db = require("../models");
+const Lesson = db.Lesson;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Lesson
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.Тема || !req.body.Дата_и_время) {
+    res.status(400).send({
+      message: "Тема and Дата и время cannot be empty!"
+    });
+    return;
+  }
+
+  // Create a Lesson
+  const lesson = {
+    Тема: req.body.Тема,
+    Дата_и_время: req.body.Дата_и_время,
+    ID_студента: req.body.ID_студента,
+    ID_курса: req.body.ID_курса
+  };
+
+  // Save Lesson in the database
+  Lesson.create(lesson)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Lesson."
+      });
+    });
+};
+
+// Retrieve all Lessons from the database.
+exports.findAll = (req, res) => {
+  const тема = req.query.тема;
+  var condition = тема ? { Тема: { [Op.like]: `%${тема}%` } } : null;
+
+  Lesson.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving lessons."
+      });
+    });
+};
+
+// Find a single Lesson with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Lesson.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Lesson with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Lesson with id=" + id
+      });
+    });
+};
+
+// Update a Lesson by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Lesson.update(req.body, {
+    where: { ID_занятия: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Lesson was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Lesson with id=${id}. Maybe Lesson was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Lesson with id=" + id
+      });
+    });
+};
+
+// Delete a Lesson with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Lesson.destroy({
+    where: { ID_занятия: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Lesson was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Lesson with id=${id}. Maybe Lesson was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Lesson with id=" + id
+      });
+    });
+};
+
+// Delete all Lessons from the database.
+exports.deleteAll = (req, res) => {
+  Lesson.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Lessons were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while removing all lessons."
+      });
+    });
+};
